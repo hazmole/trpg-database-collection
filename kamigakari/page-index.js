5cdf25a2e440073b93d6449935db0cba15e3b628
Daniel.Huang(黃主同) <Daniel.Huang@infortrend.com>
2025-10-02 18:03:30 +0800

    establish template

---
+++ kamigakari/page-index.js
@@ -0,0 +1,19 @@
+var SYSTEM_TITLE = "神我狩";
+var INDEX = [
+	{ title: "世界觀", members: [
+		{ title: "基本設定", url: "" },
+		{ title: "世界的情勢", url: "" },
+		{ title: "超常存在組織", url: "" },
+	] },
+	{ title: "遊戲規則", members: [
+		{ title: "", url: "" },
+	] },
+	{ title: "資料庫", members: [
+		{ title: "種族", url: "" },
+		{ title: "稱號", url: "" },
+		{ title: "表職業", url: "" },
+		{ title: "共通天賦", url: "" },
+		{ title: "道具", url: "" },
+		{ title: "敵人", url: "" },
+	] },
+];
---
+++ shared/js/navbar.js
@@ -0,0 +1,132 @@
+const ScreenMinWidthConfig = 640;
+var SYSTEM_TITLE, INDEX;
+
+const NavBarCtrl = {
+
+  elemColle: {},
+  pageIdxArr: [],
+
+  init: function() {
+    // Define Element ID
+    this.elemColle.headerTitleElem = document.getElementById("Header-Title");
+    this.elemColle.menuElem        = document.getElementById("Menu");
+    this.elemColle.iframeContext   = document.getElementById("Context");
+
+    // Write WebTitle
+    this.elemColle.headerTitleElem.textContent = SYSTEM_TITLE;
+    
+    // Render Navigation Menu
+    this.renderNavBar();
+    this.goToDefaultPage();
+  },
+
+  renderNavBar: function() {
+    const self = this;
+    var elemArr = INDEX.map((item, idx) => { return genNavItemElem(item, idx) });
+    this.elemColle.menuElem.innerHTML = elemArr.join('');
+
+    // ================
+    function genNavItemElem(item, idx) {
+      if (!!item.members)
+        return genNavGroupElem(item, idx);
+      else {
+        self.pageIdxArr.push("" + idx);
+        return genNavLinkElem(item, idx)
+      }
+    }
+    function genNavLinkElem(linkItem, idx) {
+      const attr = [
+        `onClick="link(this)"`,
+        `data-idx="${idx}"`
+      ];
+      return `<div class="NavLink" ${attr.join(' ')}">${linkItem.title}</div>`;
+    }
+    function genNavGroupElem(groupItem, groupIdx) {
+      const idx_prefix = groupIdx + '-';
+      const memberArr = groupItem.members.map((item, idx) => genNavItemElem(item, idx_prefix + idx));
+      return `<div class="NavGroupOuter">
+        <div class="NavGroupTitle" onClick="toggleGroup(this)">${groupItem.title}</div>
+        <div class="NavGroupInner">${memberArr.join('')}</div>
+      </div>`;
+    }
+  },
+
+  goToDefaultPage: function() {
+    // Read URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const urlPageIdx = urlParams.get('p');
+    
+    if (this.pageIdxArr.includes(urlPageIdx)) {
+      this.goToPage(urlPageIdx);
+    } else {
+      this.goToPage(this.pageIdxArr[0]);
+    }
+  },
+
+  goToPage: function(indexTicket) {
+    const idxArr = indexTicket.split("-");
+    let arrPointer = INDEX;
+    let url = "";
+    for (idx of idxArr) {
+      const item = arrPointer[parseInt(idx)];
+      if (!!item.members) arrPointer = item.members;
+      else {
+        url = item.url
+      }
+    }
+
+    // Modify Iframe
+    this.elemColle.iframeContext.src = url;
+    // Modify URL
+    const path = window.location.pathname
+    window.history.pushState({ additionalInformation: 'Updated the URL with JS' }, SYSTEM_TITLE, path+`?p=${indexTicket}`);
+
+    // find & Mark Active
+    this.markElemActive(index);
+  },
+
+  markElemActive: function(index) {
+    const elemArr = document.querySelectorAll(`[data-idx="${index}"]`);
+    if (!elemArr || elemArr.length <= 0) return ;
+    
+    // toggle Active
+    const elem = elemArr[0];
+    elem.classList.add("Active");
+    if (this.elemColle.activePageBtn) {
+      this.elemColle.activePageBtn.classList.remove("Active");
+    }
+    this.elemColle.activePageBtn = elem;
+    
+    // unfold Parent Group
+    let elemPointer = elem;
+    while (elemPointer.id !== "Menu") {
+      if (elemPointer.classList.contains("NavGroupOuter")) {
+        elemPointer.classList.add("Unfold");
+      }
+      elemPointer = elemPointer.parentElement;
+    }
+  }
+}
+
+
+/* interactive API for Elements */
+function link(elem) {
+  if (NavBarCtrl.elemColle.activePageBtn == elem) return ;
+
+  const elemIdx = elem.attributes["data-idx"].value;
+  NavBarCtrl.goToPage(elemIdx);
+}
+function toggleMenu() {
+  const navBarElem = document.getElementById("Menu");
+  toggle(navBarElem, "Unfold");
+}
+function toggleGroup(elem) {
+  toggle(elem.parentElement, "Unfold");
+}
+function toggle(elem, className) {
+  if (elem.classList.contains(className)) {
+    elem.classList.remove(className);
+  } else {
+    elem.classList.add(className);
+  }	
+}
