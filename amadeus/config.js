6a2b3b36a2af85a0f15b8ddaff15c76cbe7df932
Daniel.Huang(黃主同) <Daniel.Huang@infortrend.com>
2026-07-17 11:06:02 +0800

    [amadeus] add rule-page: fate-of-wheel & roll-dice

--- amadeus/config.js
+++ amadeus/config.js
@@ -1,24 +1,26 @@
 const INDEX = [
 	{ title: "首頁", pid: "home" },
 	{ title: "規則單元", members: [
-		{ title: "創建角色", pid: "doc-core-rule-character-creation" },
+		{ title: "1. 創建角色", pid: "doc-core-rule-character-creation" },
+		{ title: "2. 命運之輪", pid: "doc-core-rule-wheel-of-fate" },
+		{ title: "3. 行為判定", pid: "doc-core-rule-dice-check" },
 	]},
 	{ title: "玩家資料庫", members: [
-		{ title: "神群", members: [
-			{ title: "希臘神群", pid: "data-deities-greek" },
-			{ title: "大和神群", pid: "data-deities-japan" },
-			{ title: "埃及神群", pid: "data-deities-egypt" },
-			{ title: "克蘇魯神群", pid: "data-deities-cthulhu" },
-			{ title: "北歐神群", pid: "data-deities-nordic" },
-			{ title: "泰坦神群", pid: "data-deities-titan" },
-			{ title: "中華神群", pid: "data-deities-china" },
-			{ title: "凱爾特神群", pid: "data-deities-celtic" },
-			{ title: "印度神群", pid: "data-deities-india" },
-			{ title: "中美洲神群", pid: "data-deities-america" },
-		]},
+		// { title: "神群", members: [
+		// 	{ title: "希臘神群", pid: "data-deities-greek" },
+		// 	{ title: "大和神群", pid: "data-deities-japan" },
+		// 	{ title: "埃及神群", pid: "data-deities-egypt" },
+		// 	{ title: "克蘇魯神群", pid: "data-deities-cthulhu" },
+		// 	{ title: "北歐神群", pid: "data-deities-nordic" },
+		// 	{ title: "泰坦神群", pid: "data-deities-titan" },
+		// 	{ title: "中華神群", pid: "data-deities-china" },
+		// 	{ title: "凱爾特神群", pid: "data-deities-celtic" },
+		// 	{ title: "印度神群", pid: "data-deities-india" },
+		// 	{ title: "中美洲神群", pid: "data-deities-america" },
+		// ]},
 		{ title: "背景", pid: "data-background" },
-		{ title: "泛用恩惠", pid: "data-common-boon" },
-		{ title: "道具", pid: "data-item" },
+		// { title: "泛用恩惠", pid: "data-common-boon" },
+		// { title: "道具", pid: "data-item" },
 	]},
 ];
 
@@ -27,7 +29,10 @@ const MAP = {
 
 	"doc-core-rule-character-creation": {
 		title: "創建角色", handler: 'docs', url: 'docs/core-doc-rule-character-creation.json' },
-
+	"doc-core-rule-wheel-of-fate": {
+		title: "命運之輪", handler: 'docs', url: 'docs/core-doc-rule-wheel-of-fate.json' },
+	"doc-core-rule-dice-check": {
+		title: "行為判定", handler: 'docs', url: 'docs/core-doc-rule-dice-check.json' },
 	"data-background": {
 		title: "背景", handler: 'data', script: 'pages/player-background.js' },
 }
